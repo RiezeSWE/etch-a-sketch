ceaa4a74d30009b197b0bd276d18971270c97ccc
RiezeSWE <riezenfell@outlook.com>
2022-04-09 17:04:14 +0800

    Add etch function

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 //function to create the grid, embedded to body to invoke it on load of webpage
-function makeGrid() {
+function makeGrid(val) {
     //creates the amount of rows
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < val; i++) {
         let row = document.createElement('div');
         row.className = "row";
         //creates the amount of boxes per row
-        for (let j = 0; j < 16; j++) {
+        for (let i = 0; i < val; i++) {
             let box = document.createElement('div');
             box.className = "box";
             row.appendChild(box);
         };
         document.querySelector('.grid').appendChild(row);
-    };
+    }
 };
 
-makeGrid();
+//selects every box, and for each box add an event listener on mouse over to replace the box's background color with black
+function etch() {
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => {
+      box.addEventListener("mouseover", e => {
+        box.style.backgroundColor = "black";
+      });
+    });
+  }
 
-const boxes = document.querySelectorAll('.box');
-// console.log(boxes)
+//changes grid size based on user input
+function gridSize() {
+    let input = prompt("Please enter a value between 1 and 64.");
+    if ((input <= 64) && (1 <= input)) {
+      document.querySelector('.grid').innerHTML = " ";
+      makeGrid(input);
+      etch();
+    } else {
+      return alert("Error. Please enter a value between 1 and 64.")
+    }
+};
 
-//clears the grid, and then resets it with a new grid
+//clears the grid, and then resets it with the default 16x16 grid
 function resetGrid() {
     document.querySelector('.grid').innerHTML = " ";
-    makeGrid();
+    makeGrid(16);
+    etch();
 }
+
+makeGrid(16); //preloads a 16x16 grid onto the website
+etch(); //preloads the "etch" function
