f83bdebc72107915a38cdd5a33bd9d384c064a15
RiezeSWE <riezenfell@outlook.com>
2022-04-09 13:02:11 +0800

    Add grid

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+//function to create the grid, embedded to body to invoke it on load of webpage
 function makeGrid() {
-    for (let i = 0; i < 8; i++) {
+    //creates the amount of rows
+    for (let i = 0; i < 16; i++) {
         let row = document.createElement('div');
         row.className = "row";
-        for (let j = 0; j < 8; j++) {
+        //creates the amount of boxes per row
+        for (let j = 0; j < 16; j++) {
             let box = document.createElement('div');
             box.className = "box";
             row.appendChild(box);
         };
-        document.querySelector('.gridContainer').appendChild(row);
+        document.querySelector('.grid').appendChild(row);
     };
 };
+
+makeGrid();
+
+const boxes = document.querySelectorAll('.box');
+// console.log(boxes)
+
+//clears the grid, and then resets it with a new grid
+function resetGrid() {
+    document.querySelector('.grid').innerHTML = " ";
+    makeGrid();
+}
